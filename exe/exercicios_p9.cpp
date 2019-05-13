ce014ca7e49a52c26501bcf187721338b51be1b9
RajdevKumar <rajdev_kumar@hotmail.com>
2019-05-13 18:27:58 +0100

    adicionado exercicio 9

--- exe/exercicios_p9.cpp
+++ exe/exercicios_p9.cpp
@@ -1,3 +1,18 @@
+/*
+
+Escreva um programa que verifique a validade de uma senha fornecida pelo utilizador. A senha válida
+é o número 1234. Devem ser impressas as seguintes mensagens:
+- ACESSO PERMITIDO caso a senha seja válida.
+- ACESSO NEGADO caso a senha seja inválida.
+Guarde o programa com o nome Exercicio_P9.
+
+
+
+
+*/ 
+
+
+
 #include<stdio.h>
 #include<locale.h>
 #include<string.h>
