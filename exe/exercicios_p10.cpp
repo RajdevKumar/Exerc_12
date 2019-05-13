6ce6640b84f8b38162b467a351d127efe1e6556f
RajdevKumar <rajdev_kumar@hotmail.com>
2019-05-13 18:42:38 +0100

    adicionado exercicio 10

---
+++ exe/exercicios_p10.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<locale.h>
+#include<string.h>
+main(){
+	setlocale(LC_ALL,"portuguese");
+	int n_apples_compadas;
+	printf("indique o número de maças compradas: ");
+	scanf("%d",&n_apples_compadas);
+	if(n_apples_compadas<12){
+		float custo_apple =0.30;
+		float preco = n_apples_compadas*custo_apple;
+		printf("O preço pago foi %.2f",preco);
+		
+		
+	}
+	else{
+				float custo_apple =0.25;
+		float preco = n_apples_compadas*custo_apple;
+		printf("O preço pago foi %.2f",preco);
+		
+	}
+	
+}
