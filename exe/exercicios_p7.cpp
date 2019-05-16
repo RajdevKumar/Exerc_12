212998bd0a80dafd37adce01ffa72d05cfc074f1
RajdevKumar <rajdev_kumar@hotmail.com>
2019-05-16 20:20:18 +0100

    Exercicio 7

--- exe/exercicios_p7.cpp
+++ exe/exercicios_p7.cpp
@@ -8,7 +8,7 @@ main()
     scanf("%d%d%d",&numero1,&numero2,&numero3);
 
     if(numero1==numero2 && numero1==numero3)
-        printf("três são números iguais");
+        printf("os três números são iguais");
 
     else{
 
